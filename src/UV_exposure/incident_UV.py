8b58791229e5be84d351d6ecff767000fcd32b9d
Tom Harrison <harrisontom612@gmail.com>
2022-09-17 19:27:32 +0100

    Nest functions. Need to think about daylight saving hours.

--- src/UV_exposure/incident_UV.py
+++ src/UV_exposure/incident_UV.py
@@ -17,61 +17,70 @@ def metric_ang_decl():
     """
     
     day = datetime.utcnow().timetuple().tm_yday
-    return(-23.45 * math.cos((360 / 365) * (day + 10)))
+    return(-23.45 * math.cos(math.radians((360 / 365) * (day + 10))))
 
 
-def get_local_time(lat, long):
-    """
-    Returns the current local time for a given location.
-    """
-
-    # find timezone name
-    timezone_str = tzwhere.tzwhere().tzNameAt(lat, long)
+def elevation_angle(lat, long):
     
-    # find time difference of timezone compare to UTC
-    timezone = pytz.timezone(timezone_str)
-    utc_offset = timezone.utcoffset(datetime.utcnow())
-    
-    # subtract time difference from UTC
-    local_time = datetime.utcnow() + timedelta(days = utc_offset.days,
-                                               seconds = utc_offset.seconds) 
-    
-    return(local_time)
+    def get_local_time(lat, long):
+        """
+        Returns the current local time for a given location.
+        """
+        
+        # find timezone name
+        timezone_str = tzwhere.tzwhere().tzNameAt(lat, long)
+
+        # find time difference of timezone compare to UTC
+        timezone = pytz.timezone(timezone_str)
+        utc_offset = timezone.utcoffset(datetime.utcnow())
+
+        # subtract time difference from UTC
+        local_time = datetime.utcnow() + timedelta(days = utc_offset.days,
+                                                   seconds = utc_offset.seconds) 
+
+        return(local_time)
+
+    def LSTM(lat, long):
+        """
+        Returns the Local Standard Time Meridian
+        """
+
+        # local_time = get_local_time(lat = lat, long = long)
+        delta_utc =  local_time.hour - datetime.utcnow().hour
+        return(15 * delta_utc)
 
 
-def LSTM(lat, long):
-    """
-    Returns the Local Standard Time Meridian
-    """
+    def time_correct_fact(lat, long):
+        """
+        Returns the time correction factor for a particular longitude
+        """
+
+        # local_time = get_local_time(lat = lat, long = long)
+        day = local_time.timetuple().tm_yday
+        b = (360 / 365) * (day - 81)
+
+        tcf = 4 * (long - LSTM(lat = lat, long = long)) + 9.87*math.sin(math.radians(2*b)) - 7.53*math.cos(math.radians(b)) - 1.5*math.sin(math.radians(b))
+        return(tcf)
+
+
+    def hour_angle(lat, long):
+        """
+        Returns the hour angle for a particular longitude
+        """
+
+        # local_time = get_local_time(lat = lat, long = long)
+        time_corrected = local_time + timedelta(hours = -12, minutes = time_correct_fact(lat = lat, long = long) / 60, seconds = 0)
+
+        hr_angle = 15 * (time_corrected.hour + time_corrected.minute / 60 + time_corrected.second / 3600)
+
+        return(hr_angle)
     
     local_time = get_local_time(lat = lat, long = long)
-    delta_utc =  local_time.hour - datetime.utcnow().hour
-    return(15 * delta_utc)
-
-
-def time_correct_fact(lat, long):
-    """
-    Returns the time correction factor for a particular longitude
-    """
+    ang_decl = metric_ang_decl()
     
-    local_time = get_local_time(lat = lat, long = long)
-    day = local_time.timetuple().tm_yday
-    b = (360 / 365) * (day - 81)
+    elevation_angle = math.asin(math.sin(math.radians(ang_decl)) * math.sin(math.radians(lat)) + math.cos(math.radians(ang_decl)) * math.cos(math.radians(lat)) * math.cos(math.radians(hour_angle(lat = lat, long = long))))
     
-    tcf = 4*(long-LSTM(lat = lat, long = long)) + 9.87*math.sin(2*b) - 7.53*math.cos(b) - 1.5*math.sin(b)
-    return(tcf)
-
-
-def hour_angle(lat, long):
-    """
-    Returns the hour angle for a particular longitude
-    """
+    # convert elevation angle from radians to degrees
+    elevation_angle = elevation_angle * 180 / math.pi
     
-    local_time = get_local_time(lat = lat, long = long)
-    time_corrected = local_time + timedelta(hours = -12, minutes = time_correct_fact(lat = lat, long = long) / 60, seconds = 0)
-    
-    hr_angle = 15 * (time_corrected.hour + time_corrected.minute / 60 + time_corrected.second / 3600)
-    
-    return(hr_angle)
-    
-    
+    return(elevation_angle)
