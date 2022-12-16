0b1633a9bf139ed274f8526ac20d3f721a54e96f
Tom Harrison <harrisontom612@gmail.com>
2022-12-16 23:29:50 +0000

    Change to store values in an argparser

--- src/runner.py
+++ src/runner.py
@@ -9,15 +9,20 @@ import math
 import pandas as pd
 import warnings
 
-from geopy.geocoders import Nominatim
 from datetime import datetime, timedelta
+from geopy.geocoders import Nominatim
+from src import parse_args
 from utils.UV_exposure import *
+from utils.UV_exposure import get_times
 
 
-
-def runner(lat, long, location, time):
+def runner(args):
     # Find current local time and the corresponding UTC time
-    local_time, utc_time = incident_UV.get_times(lat = lat, long = long, time = time)
+    args.time, utc_time = get_times(
+        lat = args.latitude,
+        long = args.longitude,
+        time = args.time
+    )
     
     # Find the current UTC datetime. We will use it for extracting ozone data, calculating the Earth-Sun distance, and more
     day_of_year = utc_time.timetuple().tm_yday
@@ -29,13 +34,13 @@ def runner(lat, long, location, time):
     
     # Find the thickness of the ozone layer at the location of interest
     ozone_thickness = ozone.get_ozone_thickness(df_ozone = df_ozone, 
-                                                lat = lat, 
-                                                long = long)
+                                                lat = args.latitude, 
+                                                long = args.longitude)
     
     # Find the solar zenith angle at the location and time given
-    zenith = incident_UV.zenith_angle(lat = lat, 
-                                      long = long, 
-                                      local_time = local_time, 
+    zenith = incident_UV.zenith_angle(lat = args.latitude, 
+                                      long = args.longitude, 
+                                      local_time = args.time, 
                                       utc_time = utc_time)
 
     # Find the cleay-sky UV index at the specified location and time
@@ -43,41 +48,48 @@ def runner(lat, long, location, time):
                                               zenith = zenith, 
                                               tot_ozone = ozone_thickness)
 
-    # Find the cloud modification factor based on weather
-    cmf = cloud_cover.get_cloud_mod_factor(lat = lat, 
-                                           long = long, 
-                                           time = time, 
-                                           api_key = constants.weatherstack_api_key)
+    print('\nIn {} on {} at {}: \nClear-sky UV index = {}'.format(args.location, args.time.strftime("%d/%m/%Y"), args.time.strftime("%H:%M"), round(clear_sky_UVI, 2)))
     
-    real_UVI = cmf * clear_sky_UVI
+    if args.current:
+        # Find the cloud modification factor based on weather
+        cmf = cloud_cover.get_cloud_mod_factor(lat = args.latitude, 
+                                               long = args.longitude,
+                                               api_key = constants.weatherstack_api_key)
     
-    print('\nIn {} on {} at {}: \nClear-sky UV index = {} \nReal UV index = {}\n'.format(location, local_time.strftime("%d/%m/%Y"), local_time.strftime("%H:%M"), round(clear_sky_UVI, 2), round(real_UVI, 2)))
+        real_UVI = cmf * clear_sky_UVI
+        print('Real UV Index = {}\n'.format(round(real_UVI, 2)))
+    elif args.time and not args.current:
+        warnings.warn("There is currently no support for historical weather data.")
     
     return 0
 
 
 
 if __name__ == "__main__":
-    print('Where in the world are you?')
-    location = input()
-        
-    # Find the coordinates of the specified location
+
+    args = parse_args()
+    
+    # Find the coordinates of the specified location using geolocator
     geolocator = Nominatim(timeout=10, user_agent="PDS")
-    location_full = geolocator.geocode(location)
-    lat, long = location_full.latitude, location_full.longitude
-
-    print('If you want the current UV index for {}, type "Now", otherwise type the date and time that you would like the UV index for. Date and time must be in the format "day/month/year hour:minute."'.format(location))
-    time = input()
-
-    # If the user wants the current time, then find the local time of that location
-    # Otherwise convert the time into a date format
-    if time.lower() == "now":
-        time = None
-    else:
-        time = datetime.strptime(time, '%d/%m/%y %H:%M')
-
+    location_full = geolocator.geocode(args.location)
+    
+    # Store latitude and longitude in the argparser
+    args.latitude, args.longitude = location_full.latitude, location_full.longitude
+        
+    # If the user wants the current time, set time = None
+    # If the user wants a particular time, then convert the supplied string to a datetime object
+    if args.current:
+        args.time = None
+    elif args.time:
+        args.time = datetime.strptime(args.time, '%d/%m/%y %H:%M')
+    
+    # Convert start and end times to datetime objects
+    if args.start_time or args.end_time:
+        try:
+            args.start_time = datetime.strptime(args.start_time, '%d/%m/%y %H:%M')
+            args.end_time = datetime.strptime(args.end_time, '%d/%m/%y %H:%M')
+        except TypeError as exc:
+            raise Exception('Both the start time and end time have to be specified together.') from exc
+                    
     # Call the runner
-    runner(lat = lat, 
-           long = long, 
-           location = location, 
-           time = time)
+    runner(args)
--- utils/UV_exposure/incident_UV.py
+++ utils/UV_exposure/incident_UV.py
@@ -1,39 +1,6 @@
 import math
-import pytz
-import warnings
 
-from datetime import datetime, timedelta
-from tzwhere import tzwhere
-
-
-def get_times(lat, long, time = None):
-    """
-    A function to return the local time and the utc time. If the user does not enter a time then the function finds the current local time and the current utc time. If the user enter s atime, then the function returns that time and calculates what the UTC time would be, given the specified time for a particular location.
-    """
-    
-    # find timezone name
-    # we expect a warning here which is generated from the tzwhere package - suppress it.
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore")
-        timezone_str = tzwhere.tzwhere().tzNameAt(lat, long)
-
-    # find time difference between timezone and UTC
-    timezone = pytz.timezone(timezone_str)
-    utc_offset = timezone.utcoffset(datetime.utcnow())
-
-    if time is None:
-        utc_time = datetime.utcnow()
-        # add time difference to UTC time
-        local_time = utc_time + timedelta(days = utc_offset.days,
-                                          seconds = utc_offset.seconds) 
-        return(local_time, utc_time)
-
-    else:
-        # subtract time difference from UTC
-        utc_time = time - timedelta(days = utc_offset.days,
-                                    seconds = utc_offset.seconds)
-        return time, utc_time
-
+from datetime import timedelta
 
 
 def zenith_angle(lat, long, local_time, utc_time):
